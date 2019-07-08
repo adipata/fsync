dbe34aed7d0296988f684ff39d6464b1bbffc607
apata <apata@incert.lu>
2019-07-08 16:04:37 +0200

    The server

---
+++ fsync-server/src/main/java/lu/pata/fsync/fsyncserver/FsyncServerApplication.java
@@ -0,0 +1,13 @@
+package lu.pata.fsync.fsyncserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsyncServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsyncServerApplication.class, args);
+    }
+
+}
---
+++ fsync-server/src/test/java/lu/pata/fsync/fsyncserver/FsyncServerApplicationTests.java
@@ -0,0 +1,16 @@
+package lu.pata.fsync.fsyncserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FsyncServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
